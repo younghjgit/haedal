0320dae2555f23d1167aa5de5f018e6f1293b564
younghjgit <younghjgit@.com>
2021-06-20 16:11:01 +0900

    hw7

---
+++ 7/me.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "2018113534 전자공학부 정영훈"
+
+@app.route("/me/")
+def mee():
+    menu = ["chicken.jpg", "dog.jpg", "knu.jpg"]
+    pickme = random.choice(menu)
+    return render_template('index.html', img=pickme)
+
+if __name__ == "__main__":
+    app.run(debug=True)
